0185cb6b90a6147d53507059b45418d28009f24a
khalil Hussaini <107269671+khalilhussaini@users.noreply.github.com>
2022-10-27 19:04:41 +0200

    Add files via upload
    i finished

---
+++ script.js
@@ -0,0 +1,14 @@
+var compartment= prompt("Enter a Number between 1 and 5 ");
+
+    if (compartment == 1) {
+        console.log("Your son is in a compartment with a smaller number");
+    } else if (compartment == 2) {
+       console.log("Your son is in a compartment with a larger number"); 
+    } else if (compartment == 3) {
+       console("You are in compartment 3 but your Sonn is not here search continue to find"); 
+    } else if (compartment == 4) { 
+       console.log("You are in compartment4 but your Sonn is not here search continue to find") ;
+    } else if (compartment == 5) {
+      console.log("Yes, you found your son congratulation!.");
+    } else
+       console.log("We are Sorry unfotunately we can't fund your Sonn Search compartment form 1 to 5  mayba you can find  ");
